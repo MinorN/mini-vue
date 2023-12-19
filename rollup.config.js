367e7deac5db2c8b20a467526d3e2b7b54257310
qijiajun <1481700559@qq.com>
2023-12-20 00:13:46 +0800

    feat: 配置rollup项目打包

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
+export default [
+  {
+    // 入口文件
+    input: 'packages/vue/src/index.ts',
+    // 打包出口
+    output: [
+      // 导出 iife 模式的包
+      {
+        // 开启 SourceMap
+        sourcemap: true,
+        // 导出的文件地址
+        file: './packages/vue/dist/vue.js',
+        // 生成的包格式：一个自动执行的功能，适合作为<script>标签
+        format: 'iife',
+        // 变量名
+        name: 'Vue'
+      }
+    ],
+    // 插件
+    plugins: [
+      // ts 支持
+      typescript({
+        sourceMap: true
+      }),
+      // 模块导入的路径补全
+      resolve(),
+      // 将 CommonJS 模块转换为 ES2015
+      commonjs()
+    ]
+  }
+]
