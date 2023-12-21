acf02702b64d60f6303d46432a3b661c638481e0
qijiajun <1481700559@qq.com>
2023-12-21 21:40:30 +0800

    feat: 实现reactive直接返回Proxy

---
+++ packages/reactivity/src/reactive.ts
@@ -0,0 +1,29 @@
+import { mutableHandlers } from './baseHandlers'
+
+
+export const reactiveMap = new WeakMap<object, any>()
+
+
+export function reactive(target: object) {
+    return createReactiveObject(target, mutableHandlers, reactiveMap)
+}
+
+function createReactiveObject(
+    target: object,
+    baseHandlers: ProxyHandler<any>,
+    proxyMap: WeakMap<object, any>
+) {
+
+    // 这里是一个缓存，获取到了直接返回
+    const existProxy = proxyMap.get(target)
+    if (existProxy) {
+        return existProxy
+    }
+    // 创建代理
+    const proxy = new Proxy(target, baseHandlers)
+
+    // 将该实例缓存
+    proxyMap.set(target, proxy)
+
+    return proxy
+}
