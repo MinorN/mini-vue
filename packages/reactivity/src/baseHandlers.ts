d4a42f16e04d853fb21ecf5c17c9887aa603321a
qijiajun <1481700559@qq.com>
2023-12-21 23:37:09 +0800

    feat: 添加mutableHandlers

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,1 +1,18 @@
-export const mutableHandlers: ProxyHandler<object> = {}
+
+export const mutableHandlers: ProxyHandler<object> = {
+    // 核心是 get，set 方法
+    get(target: object, key: string | symbol, receiver: object) {
+        const res = Reflect.get(target, key, receiver)
+        // 需要进行 依赖收集
+        track(target, key)
+
+        return res
+    },
+    set(target: object, key: string | symbol, value: unknown, receiver: object) {
+        const res = Reflect.set(target, key, value, receiver)
+        // 需要 触发依赖
+        trigger(target, key, value)
+
+        return res
+    },
+}
