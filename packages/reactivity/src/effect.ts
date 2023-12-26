c8da74416c4b2130c1ad5a0e997b8dcae449a330
qijiajun <1481700559@qq.com>
2023-12-26 23:14:30 +0800

    feat: 添加effect并导出

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,3 +1,30 @@
+
+
+
+
+export function effect<T = any>(fn: () => T) {
+    const _effect = new ReactiveEffect(fn)
+
+    _effect.run()
+}
+// 当前激活的实例
+export let activeEffect: ReactiveEffect | undefined
+
+// reactive effect 实例
+export class ReactiveEffect<T = any>{
+    constructor(public fn: () => T) { }
+
+    run() {
+        // run 函数就是触发了传入的回调函数
+
+        // 但是之前需要进行处理
+        activeEffect = this
+
+        return this.fn()
+    }
+}
+
+
 export function track(target: object, key: unknown) {
 
 }
