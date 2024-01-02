5c4ba39378f9f00622af52c5b75386810af20a26
qijiajun <1481700559@qq.com>
2024-01-02 23:27:02 +0800

    feat: 添加ref对复杂类型的支持

---
+++ packages/reactivity/src/ref.ts
@@ -0,0 +1,54 @@
+import { Dep, createDep } from "./dep"
+import { activeEffect, trackEffects } from "./effect"
+import { toReactive } from './reactive'
+
+export interface Ref<T = any> {
+    value: T
+}
+
+export function ref(value?: unknown) {
+    return createRef(value, false)
+}
+
+function createRef(rawValue: unknown, shallow: boolean) {
+    if (isRef(rawValue)) {
+        // 如果当前传入的是 ref 直接返回
+        return rawValue
+    }
+
+    return new RefImpl(rawValue, shallow)
+}
+
+class RefImpl<T>{
+    private _value: T
+
+    public dep?: Dep = undefined
+
+    public readonly __v_isRef = true
+
+    constructor(value: T, public readonly __v_isShallow: boolean) {
+        this._value = __v_isShallow ? value : toReactive(value)
+    }
+
+    get value() {
+        trackRefValue(this)
+        return this._value
+    }
+
+    set value(value) {
+
+    }
+}
+
+
+export function isRef(r: any): r is Ref {
+    // 判断当前是否是ref
+    return !!(r && r.__v_isRef === true)
+}
+
+export function trackRefValue(ref) {
+    if (activeEffect) {
+        // 当前存在 activeEffect，把当前的副作用都放入岛 ref.dep 中
+        trackEffects(ref.dep || (ref.dep = createDep()))
+    }
+}
